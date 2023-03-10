7e37d7cbd40780bfe06aa26a4db44c383acba800
Mayank303030 <92243054+Mayank303030@users.noreply.github.com>
2023-03-10 19:59:31 +0000

    Add files via upload

---
+++ m.js
@@ -0,0 +1,11 @@
+// Define the button element
+const addStoresBtn = document.querySelector('#add-stores-btn');
+
+// Define a function to execute when the button is clicked
+function addStores() {
+  // Code to add stores goes here
+  console.log('Stores added!');
+}
+
+// Attach the function to the button's click event
+addStoresBtn.addEventListener('click', addStores);
